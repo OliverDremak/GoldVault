4af2e3fce705d1e0385e9f6ea5efaef12f5376e7
OliverDremak <dremakoliver@gmail.com>
2025-01-14 21:39:59 +0100

    Android Status bar changed

--- GoldVault/Platforms/Android/MainActivity.cs
+++ GoldVault/Platforms/Android/MainActivity.cs
@@ -7,5 +7,14 @@ namespace GoldVault
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#2E5077"));
+            }
+        }
+
     }
 }
